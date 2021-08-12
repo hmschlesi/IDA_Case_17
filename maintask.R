951e0a2fe4d1b934ffeede64d8720b20a9d7e4d2
hmschlesi <hmschlesi@googlemail.com>
2021-08-12 16:32:40 +0200

    maintask

--- maintask.R
+++ maintask.R
@@ -1,5 +1,5 @@
 #main task collect all car ids for failed cars/parts/components
-#K2 component file is already fixed!!!
+#T2 component file is already fixed!!!
 install.packages("readr")
 library("readr")
 install.packages("stringr")
@@ -7,3 +7,14 @@ library("stringr")
 install.packages("tidyverse")
 library("tidyverse")
 
+Path_T2_1 <- '03_Data/Einzelteil/Einzelteil_T02_p1.txt'
+Path_T2_2 <- '03_Data/Einzelteil/Einzelteil_T02_p2.txt'
+
+T2_1 <- read_table(Path_T2_1)
+T2_2 <- read_table(Path_T2_2)
+
+Path_K1BE1_kom <- '03_Data/Komponente/Bestandteile_Komponente_K1BE1.csv'
+Path_K1BE2_kom <- '03_Data/Komponente/Bestandteile_Komponente_K1BE2.csv' 
+
+K1BE1_kom <- read_csv2(Path_K1BE1_kom)
+K1BE2_kom <- read_csv2(Path_K1BE2_kom)
