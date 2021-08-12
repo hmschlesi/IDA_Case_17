1d59688fcd4276a3545cc76b7059ce8b379d7ec5
hmschlesi <hmschlesi@googlemail.com>
2021-08-12 16:16:18 +0200

    e# Please enter the commit message for your changes. Lines starting

---
+++ maintask.R
@@ -0,0 +1,9 @@
+#main task collect all car ids for failed cars/parts/components
+#K2 component file is already fixed!!!
+install.packages("readr")
+library("readr")
+install.packages("stringr")
+library("stringr")
+install.packages("tidyverse")
+library("tidyverse")
+
