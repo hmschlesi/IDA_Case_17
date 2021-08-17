af3fad3b360256dd758e7a82fa54faaec28556ff
hmschlesi <hmschlesi@googlemail.com>
2021-08-17 19:11:45 +0200

    main mit Reperaturdatum und Bundesland

--- maintask.R
+++ maintask.R
@@ -6,6 +6,11 @@ install.packages("stringr")
 library("stringr")
 install.packages("tidyverse")
 library("tidyverse")
+library("readxl")
+library("lubridate")
+
+start_repair <- "2021-09-01"
+n_day=1
 
 Path_T2_1 <- '03_Data/Einzelteil/Einzelteil_T02_p1.txt'
 Path_T2_2 <- '03_Data/Einzelteil/Einzelteil_T02_p2.txt'
@@ -75,6 +80,9 @@ T2_cars<- cars%>%
   left_join(Zul, by=c("ID_Fahrzeug"="IDNummer"))%>%
   select(-8)
 
+#-----------------------------------------------#
+#some plots for better understanding of the data ser
+
 T2_cars_group_by_oem <- T2_cars %>%
   mutate(OEM=str_sub(ID_Fahrzeug,4,4)) %>%
   mutate(affected=as_factor(affected))%>%
@@ -107,17 +115,69 @@ ggplot(T2_cars_by_region,aes(x=reorder(Gemeinden,-n),y=n))+
   theme_bw()+
   ggtitle("The ten most affected regions with defect component T2")
 
+#---------------------------------------------------#
+#End of general plots
 
+#Connecting the geo data to vehicel data
 
 Path_geo <- '03_Data/Geodaten/Geodaten_Gemeinden_v1.2_2017-08-22_TrR.csv'
+Path_geo2 <- '03_Data/Geodaten/PLZ.csv'
+Path_Bundes <- '03_Data//Geodaten/Liste-der-PLZ-in-Excel-Karte-Deutschland-Postleitzahlen (1).xlsx'
+# A quick quiz about german state capitals :)
+# four states dont have any afffected cars, i.g. Berlin, Bremen, ...
+bundes_haupt <- data.frame(Bundesland=c("Sachsen","Nordrhein-Westfalen" ,"Brandenburg","Thüringen" ,"Sachsen-Anhalt", "Mecklenburg-Vorpommern", "Niedersachsen", "Schleswig-Holstein" ,"Hamburg", "Hessen", "Rheinland-Pfalz", "Bayern", "Baden-Württemberg", "Saarland" ),
+                           Bundeshaupt=c("Dresden", "Düsseldorf", "Potsdam", "Erfurt", "Magdeburg", "Schwerin" , "Hannover" , "Kiel", "Hamburg","Wiesbaden", "Mainz", "München", "Stuttgart" , "Saarbrücken"))
+
+
+bundes <- read_excel(Path_Bundes)
+
+geo2<- read_table(Path_geo2)
+geo2 <- geo2 %>%
+  group_by(Ort)%>%
+  summarise(lon=mean(lon),lat=mean(lat))
+
+bundes_haupt <- bundes_haupt%>%
+  left_join(geo2, by=c("Bundeshaupt"="Ort"))
+
 
 geo<-read_csv2(Path_geo)
+# Gemeinde Seeg taucht zwar in zuLassungsdaten auf, hat aber keine Referenz in den Geodaten. Seeg ist eine Gemeinede in Bayern mit der PLZ 87637 unter der PLZ ist 
+#beid en Geo daten nur ein NA als Gemeinde eingetrage. Dies wird manuell korrigiert.
+#Man hätte auch den original Datensatz von DBGeoPLz verwenden können :)
+geo <- geo %>%
+  mutate(Gemeinde=replace(Gemeinde, Postleitzahl==87637 & is.na(Gemeinde),"SEEG"))%>%
+  select(c(-1,-X))
+#euclidean Distance for distance to state capital
+eu_dist <- function(p1,p2,q1,q2){
+  d<- sqrt((q1-p1)^2+(q2-p2)^2)
+  return(d)
+}
 
-T2_cars_geo<- T2_cars %>%
+schedule_rep <- function(d){
+  ceiling(nrow(distance)/100)
+}
+
+
+#Geodaten und Bundesländer mit den Fahrzeug verknüpfen über die Zualssung und PLZ. 
+# Amd alot of other stuff
+T2_cars_bund  <- T2_cars %>%
   filter(affected=="affected")%>%
-  group_by(Gemeinden)%>%
-  summarise(n=n())%>%
-  left_join(geo, by=c("Gemeinden"="Gemeinde"))
+  left_join(geo, by=c("Gemeinden"="Gemeinde"))%>%
+  left_join(bundes, by=c("Postleitzahl"="PLZ"))%>%
+  select(c(-Kreis, -Typ))%>%
+  left_join(bundes_haupt)%>%
+  mutate(Bundesland=as_factor(Bundesland))%>%
+  mutate(distance=eu_dist( Laengengrad, Breitengrad ,lon,lat))%>%
+  group_by(Bundesland)%>%
+  arrange(Bundesland,distance)%>%
+  mutate(days=ceiling(row_number()/100))%>%
+  mutate(rep_date= date(start_repair)+days)
+
+
+T2_cars_bund_general <- T2_cars_bund %>%
+  group_by(Bundesland)%>%
+  summarise(n=n(), repair=max(days))
+
 
 
 
